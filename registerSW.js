4076c2e2bf3c9e736f0c8045c8cc1a4b9247866a
Dan Tilden <tilden@outlook.com>
2025-01-04 17:01:10 +0900

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/systemnote-pro/sw.js', { scope: '/systemnote-pro/' })})}
