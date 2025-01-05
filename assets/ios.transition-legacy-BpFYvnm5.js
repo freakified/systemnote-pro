46cba72a1ed38f443ba37193be629544908a5de9
Dan Tilden <tilden@outlook.com>
2025-01-05 10:23:34 +0900

    Updates

--- assets/ios.transition-legacy-BpFYvnm5.js
+++ assets/ios.transition-legacy-BpFYvnm5.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CP8eCzd0.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BU7uTMF-.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/md.transition-legacy-htvCDJWV.js
+++ assets/md.transition-legacy-htvCDJWV.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CP8eCzd0.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-BU7uTMF-.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
