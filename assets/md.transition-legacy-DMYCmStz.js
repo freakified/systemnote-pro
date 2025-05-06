c55fd06d876a93c1ea13f10a6b8d94a0464bc75d
Dan Tilden <tilden@outlook.com>
2025-05-06 12:14:33 +0900

    Updates

--- assets/md.transition-legacy-DMYCmStz.js
+++ assets/md.transition-legacy-DMYCmStz.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BGAjBrL9.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-BFyu4nSi.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
