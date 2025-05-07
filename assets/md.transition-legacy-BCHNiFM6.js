44169218082ab1fe6db256fbab26eecc32e31900
Dan Tilden <tilden@outlook.com>
2025-05-07 23:52:32 +0900

    Updates

--- assets/md.transition-legacy-BCHNiFM6.js
+++ assets/md.transition-legacy-BCHNiFM6.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DY3K_liW.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-BjjHNEez.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
