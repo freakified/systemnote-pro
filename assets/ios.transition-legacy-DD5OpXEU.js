1f45ed36159070e53d6e0fed41be538517ecc648
Dan Tilden <tilden@outlook.com>
2025-05-08 06:44:37 +0900

    Updates

--- assets/ios.transition-legacy-DD5OpXEU.js
+++ assets/ios.transition-legacy-DD5OpXEU.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BjjHNEez.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DbgH92KH.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
