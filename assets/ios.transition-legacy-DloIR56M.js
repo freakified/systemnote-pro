53343ad28686ebcd9c970edcb6f57fa3c6cf4dc5
Dan Tilden <freakified@gmail.com>
2026-04-27 12:14:27 +0900

    Updates

--- assets/ios.transition-legacy-DloIR56M.js
+++ assets/ios.transition-legacy-DloIR56M.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Byo96DUI.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-CFuAws4A.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
