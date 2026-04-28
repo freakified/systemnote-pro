6fd905532396c218ea20be82df5a6365504df352
Dan Tilden <freakified@gmail.com>
2026-04-28 20:58:40 +0900

    Updates

--- assets/ios.transition-legacy-BtgKGqvW.js
+++ assets/ios.transition-legacy-BtgKGqvW.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Ck7VZYPW.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-D6rUQP-s.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
